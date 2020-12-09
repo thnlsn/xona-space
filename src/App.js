75b5aa92b24c701d08fa978c2042ea97cc41098c
Thomas Nelson <thnlsn@gmail.com>
2020-12-09 00:45:16 -0800

    added initial details to starter files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './css/style.css';
 
 function App() {
-  return <div className='App'>▓▓▓▓▓Test▓▓▓▓▓</div>;
+  return <div className='App'>Welcome to Xona Space Systems</div>;
 }
 
 export default App;
