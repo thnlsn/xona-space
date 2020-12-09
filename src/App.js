08848c70c09ca0a8faacaff5e0f46f0f72d32408
Thomas Nelson <thnlsn@gmail.com>
2020-12-09 12:09:56 -0800

    fonts commented for ease of reference and navigation component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './css/style.css';
 
+import Navigation from './components/Navigation';
+
 function App() {
   return <div className='App'>Welcome to Xona Space Systems</div>;
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav className='navigation'>
+      <div className='brand'></div>
+      <ul className='navigation__nav'>
+        <li className='navigation__item'></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
