ac95f6144da8bd34fc83846ff82f1bfb21f96c46
Thomas Nelson <thnlsn@gmail.com>
2020-12-09 19:55:37 -0800

    updated selection color and attempted to display image in home component

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Home = () => {
-  return <main className='main home'>Home</main>;
+  return (
+    <main className='main home'>
+      <div className='home-banner'></div>
+    </main>
+  );
 };
 
 export default Home;
