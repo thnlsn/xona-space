7571ee5f4621cf41ae04c52ee3c1c76b90a34126
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-12-14 13:03:54 -0800

    Updates

---
+++ precache-manifest.9b685ec6b40c3955f387509d3a476f63.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5110f0b6479bf0169f8f93d591e901b5",
+    "url": "/xona-space/index.html"
+  },
+  {
+    "revision": "c83559e6c1d1fbdc9372",
+    "url": "/xona-space/static/css/main.87aaac33.chunk.css"
+  },
+  {
+    "revision": "fb35c10a36aa5f6ebc57",
+    "url": "/xona-space/static/js/2.2a83aac1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/xona-space/static/js/2.2a83aac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83559e6c1d1fbdc9372",
+    "url": "/xona-space/static/js/main.9911a91f.chunk.js"
+  },
+  {
+    "revision": "0ea8c7b1750bdf00de0f",
+    "url": "/xona-space/static/js/runtime-main.b98b0967.js"
+  },
+  {
+    "revision": "912813003d743c92f18309aead8475ca",
+    "url": "/xona-space/static/media/aerial-mobility.91281300.jpg"
+  },
+  {
+    "revision": "54ed7e6042bbc7caf1de486b44b8f6c5",
+    "url": "/xona-space/static/media/all-weather-autonomy.54ed7e60.jpg"
+  },
+  {
+    "revision": "550d77b93bb9581e7fc3ee049691bf19",
+    "url": "/xona-space/static/media/critical-infrastructure.550d77b9.jpg"
+  },
+  {
+    "revision": "befe32944c6e2d0812b65f4e9423f25c",
+    "url": "/xona-space/static/media/earth.befe3294.jpg"
+  },
+  {
+    "revision": "11ff33f8389df8632249c2da73ecc416",
+    "url": "/xona-space/static/media/maritime.11ff33f8.jpg"
+  },
+  {
+    "revision": "a40b8b09bba88089e28ac3b23276c9db",
+    "url": "/xona-space/static/media/sky-large.a40b8b09.jpg"
+  },
+  {
+    "revision": "5cac1830bfffa70225f75eb65e555de6",
+    "url": "/xona-space/static/media/smart-agriculture.5cac1830.jpg"
+  },
+  {
+    "revision": "b4688aaf471116296f9af97d57346a49",
+    "url": "/xona-space/static/media/weather-data.b4688aaf.jpg"
+  }
+]);
