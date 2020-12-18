56b4d0d7a6e16667b901c79131e0b4bc97ae7459
Thomas Nelson <thnlsn@gmail.com>
2020-12-18 02:33:29 -0800

    xona pulsar section completely component based finished

--- src/components/Pages/HomeComponents/PulsarAbility.js
+++ src/components/Pages/HomeComponents/PulsarAbility.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const PulsarAbility = ({ ability }) => {
+  // ability: ['description', 'image']
   return (
     <div className='xona-pulsar__ability' data-aos='fade-up'>
-      <img src={benefits[0][1]} alt='Pulsar Encryption' />
-      <div className='xona-pulsar__image-description'>
-        Encryption & Precision Corrections
-      </div>
+      <img src={ability[1]} alt='Pulsar Encryption' />
+      <div className='xona-pulsar__image-description'>{ability[0]}</div>
     </div>
   );
 };
--- src/components/Pages/HomeComponents/XonaPulsar.js
+++ src/components/Pages/HomeComponents/XonaPulsar.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import Ability from './PulsarAbility';
 
 const XonaPulsar = ({ icon, name, description, abilities }) => {
   return (
@@ -14,22 +15,9 @@ const XonaPulsar = ({ icon, name, description, abilities }) => {
         <div className='xona-pulsar__text'>{description}</div>
       </div>
       <div className='xona-pulsar__images'>
-        <div className='xona-pulsar__ability' data-aos='fade-up'>
-          <img src={abilities[0][1]} alt='Pulsar Encryption' />
-          <div className='xona-pulsar__image-description'>
-            Encryption & Precision Corrections
-          </div>
-        </div>
-        <div className='xona-pulsar__ability' data-aos='fade-up'>
-          <img src={abilities[1][1]} alt='Pulsar Strength' />
-          <div className='xona-pulsar__image-description'>Stronger Signals</div>
-        </div>
-        <div className='xona-pulsar__ability' data-aos='fade-up'>
-          <img src={abilities[2][1]} alt='Pulsar Convergence' />
-          <div className='xona-pulsar__image-description'>
-            Faster Convergence
-          </div>
-        </div>
+        {abilities.map((ability) => (
+          <Ability ability={ability} />
+        ))}
       </div>
     </Fragment>
   );
