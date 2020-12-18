fbc2f307e02ed875b325f037bf1c04caca294b28
Thomas Nelson <thnlsn@gmail.com>
2020-12-18 02:24:29 -0800

    new xona pulsar component correctly rendering given props from db

--- src/components/Pages/HomeComponents/XonaPulsar.js
+++ src/components/Pages/HomeComponents/XonaPulsar.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 
-const XonaPulsar = ({}) => {
+const XonaPulsar = ({ icon, name, description, benefits }) => {
   return (
     <Fragment>
       <div className='xona-pulsar__banner' data-aos='fade-up'>
@@ -8,27 +8,24 @@ const XonaPulsar = ({}) => {
           <div className='xona-pulsar__icon'>
             <img src={icon} alt='Xona Icon' />
           </div>
-          xona pulsar<span>&trade;</span>
+          {name}
+          <span>&trade;</span>
         </div>
-        <div className='xona-pulsar__text'>
-          With satellites in low Earth orbit, Xona's Pulsar™ Position,
-          Navigation and Timing (PNT) service will provide the security,
-          availability and accuracy needed to support the age of autonomy.
-        </div>
+        <div className='xona-pulsar__text'>{description}</div>
       </div>
       <div className='xona-pulsar__images'>
         <div className='xona-pulsar__application' data-aos='fade-up'>
-          <img src={pulsar.benefits[0][1]} alt='Pulsar Encryption' />
+          <img src={benefits[0][1]} alt='Pulsar Encryption' />
           <div className='xona-pulsar__image-description'>
             Encryption & Precision Corrections
           </div>
         </div>
         <div className='xona-pulsar__application' data-aos='fade-up'>
-          <img src={pulsar.benefits[1][1]} alt='Pulsar Strength' />
+          <img src={benefits[1][1]} alt='Pulsar Strength' />
           <div className='xona-pulsar__image-description'>Stronger Signals</div>
         </div>
         <div className='xona-pulsar__application' data-aos='fade-up'>
-          <img src={pulsar.benefits[2][1]} alt='Pulsar Convergence' />
+          <img src={benefits[2][1]} alt='Pulsar Convergence' />
           <div className='xona-pulsar__image-description'>
             Faster Convergence
           </div>
