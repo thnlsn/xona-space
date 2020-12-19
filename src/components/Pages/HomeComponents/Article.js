1e439f2a3d4ba036228cb8ab053df95c37da7c76
Thomas Nelson <thnlsn@gmail.com>
2020-12-19 15:55:57 -0800

    created component sass file for a reusable button to use throughout the site

--- src/components/Pages/HomeComponents/Article.js
+++ src/components/Pages/HomeComponents/Article.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const Article = ({ date, image, title, description }) => {
+const Article = ({ date, image, title, description, url }) => {
   return (
     <div className='article'>
       <div className='article__date'>{date}</div>
       <img src={image} alt='Article' className='article__image' />
       <div className='article__title'>{title}</div>
       <div className='article__description'>{description}</div>
+
+      <a href='#' className='btn btn--white btn--animated'>
+        Read more
+      </a>
     </div>
   );
 };
