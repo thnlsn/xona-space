adc0a38830ff6f635fd447b12b34ac49f6f029bd
Thomas Nelson <thnlsn@gmail.com>
2020-12-22 15:30:26 -0800

    footer styles mvp for now

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,19 +9,20 @@ const { iconWhite } = logos;
 const Footer = ({ socials }) => {
   return (
     <footer className='footer'>
-      <img src={iconWhite} alt='Xona Space' className='footer__logo' />
       <div className='footer__container'>
         <div className='footer__section'>
-          <div className='footer__heading'>Contact us</div>
+          <div className='footer__heading u-marbot-small'>Contact us</div>
           <div className='footer__item'>157 South Blvd</div>
           <div className='footer__item'>San Mateo, CA 94402</div>
-          <div className='footer__item'>info@xonaspace.com</div>
+          <div className='footer__item u-martop-small'>info@xonaspace.com</div>
         </div>
         <div className='footer__section'>
-          <div className='footer__heading'>Subscribe to our newsletter</div>
+          <div className='footer__heading u-marbot-small'>
+            Subscribe to our newsletter
+          </div>
           <form action='#' className='subscribe-form'>
             <input
-              type='text'
+              type='email'
               placeholder='Your email'
               className='subscribe-form__input'
             />
