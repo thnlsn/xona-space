e645ec5c378eab03e9c8b95f3b41cb1c283cf838
Thomas Nelson <thnlsn@gmail.com>
2020-12-26 15:16:18 -0800

    added google maps embed to the contact page

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className='contact'>
+      <iframe
+        className='google-maps'
+        frameborder='0'
+        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=Xona+Space+Systems,San+WA`}
+        allowfullscreen='true'
+      ></iframe>
+    </div>
+  );
 };
 
 export default Contact;
