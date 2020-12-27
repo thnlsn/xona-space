c98225e935a4252cd51b888b0695c3ff34dd0ebf
Thomas Nelson <thnlsn@gmail.com>
2020-12-26 16:44:56 -0800

    api key for google maps in a .env for security, and contact page banner structure built

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -3,12 +3,28 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className='contact'>
-      <iframe
-        className='google-maps'
-        frameborder='0'
-        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=Xona+Space+Systems,San+WA`}
-        allowfullscreen='true'
-      ></iframe>
+      <div className='contact-banner'>
+        <iframe
+          className='google-maps'
+          frameborder='0'
+          src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=Xona+Space+Systems`}
+          allowfullscreen='true'
+        ></iframe>
+        <div className='contact-banner__info'>
+          <div className='contact-banner__heading'>Get in touch</div>
+          <div className='contact-banner__address-container'>
+            <div className='contact-banner__address-heading'>Address</div>
+            <div className='contact-banner__address'>
+              <span>157 South Blvd</span>
+              <span>San Mateo, CA 94402</span>
+            </div>
+          </div>
+          <div className='contact-banner__socials-container'>
+            <div className='contact-banner__socials-heading'>Follow Us</div>
+            <div className='contact-banner__socials-links'></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
