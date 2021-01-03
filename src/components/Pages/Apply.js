723ffac29322b7c823e305acf882f00d64bbbf48
Thomas Nelson <thnlsn@gmail.com>
2021-01-02 21:29:03 -0800

    initialized doc with spreadhseet id

--- src/components/Pages/Apply.js
+++ src/components/Pages/Apply.js
@@ -13,6 +13,8 @@ const SHEET_ID = process.env.REACT_APP_SHEET_ID;
 const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
 const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
 
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+
 // This component needs the ID of the job.
 // With that, it will find the job with the matching ID and display the application questions, etc.
 
