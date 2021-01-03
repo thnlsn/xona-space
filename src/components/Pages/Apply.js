9b2b412e7ffff1078217d2767562251db9d46f96
Thomas Nelson <thnlsn@gmail.com>
2021-01-03 13:36:09 -0800

    removed testing code for the spreadsheet interaction

--- src/components/Pages/Apply.js
+++ src/components/Pages/Apply.js
@@ -19,60 +19,44 @@ const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY.replace(
 // Initializing the sheet with the spreadsheet ID
 const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
 
-const addPosition = async (row) => {
-  try {
-    await doc.useServiceAccountAuth({
-      client_email: CLIENT_EMAIL,
-      // Sanitizing the key removed the begin and end markers, so we add that back into the string
-      private_key: `-----BEGIN PRIVATE KEY-----${PRIVATE_KEY}-----END PRIVATE KEY-----`,
-    });
-    // loads document properties and worksheets
-    await doc.loadInfo();
-    console.log(doc.title);
-
-    const sheet = doc.sheetsById[SHEET_ID];
-    const result = await sheet.addRow(row);
-    console.log(sheet.title);
-    console.log(sheet.rowCount);
-
-    const rows = await sheet.getRows();
-    console.log(rows);
-  } catch (error) {
-    console.error('Error: ', error);
-  }
-};
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const Apply = () => {
-  // Get the current location as the url path
-  const location = useLocation().pathname;
-  // Splice it into just the last item, i.e. the job ID
-  const getJobId = (path) => path.substring(path.lastIndexOf('/') + 1);
-
+  const location = useLocation().pathname; // Get the current location as the url path
+  const getJobId = (path) => path.substring(path.lastIndexOf('/') + 1); // Splice it into just the last item, i.e. the job ID
   const findJob = (id) => {
-    // Find in the positions array, the job listing with the same ID as the ending item in the url path
-    return positions.find((position) => position._uid === id);
-  };
-
-  // Get the job posting from the db where the path matches the uid
+    return positions.find((position) => position._uid === id); // Find in the positions array, the job listing with the same ID as the ending item in the url path
+  }; // Get the job posting from the db where the path matches the uid
   const job = findJob(getJobId(location));
 
   // Initialize state for the current job accessed as null
-  const [applicationData, setApplicationData] = useState(positions[1]); // STATIC TESTING OF READING JOB DATA FROM DB
+  const [applicationData] = useState(job); // Set state to the current job
 
   useEffect(() => {
-    // Set state to the object of the specific job posting
-    //--1) location is the full url path
-    //--2) GetJobId uses location and is returned as the last item in the path
-    //--3) Then findJob uses that, which is the id, to find the job with that id, and returns all its information
-    const newPosition = {
-      _uid: 'JrWbDev1',
-      title: 'Jr. Web Developer',
-      area: 'Engineering',
-      location: 'San Mateo, CA',
-      timeCommitment: 'Part-time',
-    };
+    const addPosition = async (row) => {
+      try {
+        await doc.useServiceAccountAuth({
+          client_email: CLIENT_EMAIL,
+          // Sanitizing the key removed the begin and end markers, so we add that back into the string
+          private_key: `-----BEGIN PRIVATE KEY-----${PRIVATE_KEY}-----END PRIVATE KEY-----`,
+        });
+        // loads document properties and worksheets
+        await doc.loadInfo();
+        console.log(doc.title);
 
-    addPosition(newPosition);
+        const sheet = doc.sheetsById[SHEET_ID];
+        const result = await sheet.addRow(row);
+        console.log(sheet.title);
+        console.log(sheet.rowCount);
+
+        const rows = await sheet.getRows();
+        console.log(rows);
+      } catch (error) {
+        console.error('Error: ', error);
+      }
+    };
   }, []);
 
   return (
@@ -370,3 +354,17 @@ const Apply = () => {
 };
 
 export default Apply;
+
+/*     const newPosition = {
+      _uid: 'JrWbDev1',
+      title: 'Jr. Web Developer',
+      area: 'Engineering',
+      location: 'San Mateo, CA',
+      timeCommitment: 'Part-time',
+    };
+
+    addPosition(newPosition); */
+// Set state to the object of the specific job posting
+//--1) location is the full url path
+//--2) GetJobId uses location and is returned as the last item in the path
+//--3) Then findJob uses that, which is the id, to find the job with that id, and returns all its information
